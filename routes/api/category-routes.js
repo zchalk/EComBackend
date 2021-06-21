200ae3cd64219c1e663549dcf6a45069bad5745d
zchalk <you@example.com>
2021-06-20 20:26:02 -0400

    added index model and two category get routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,13 +4,25 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({include: [{model: Product}], });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+ try{
+   const categoryData = await Category.findById(req.params.id, {include: [{ model: Product}], });
+   if (!categoryData) {
+     res.status(404).json({message:'No category with this id was found.'});
+     return;
+   }
+   res.status(200).json(categoryData);
+ } catch (err) {
+   res.status(500).json(err);
+ }
 });
 
 router.post('/', (req, res) => {
